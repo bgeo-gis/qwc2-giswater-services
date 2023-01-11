b70f1d19398b8f6bf0448fc3b2f771f88884a642
smaspons <smaspons@bgeo.es>
2023-01-11 11:19:54 +0000

    Date selector: Change endpoints to `/getdialog`, `/getdates`, `/setdates`

--- gw-dateselector-service/server.py
+++ gw-dateselector-service/server.py
@@ -1,8 +1,12 @@
+from datetime import date
 from typing import List, Optional
 from xml.dom.minidom import parseString, Document, Element
 
 from flask import Flask, request, jsonify, Response
 from flask_restx import Resource, reqparse
+import os
+import traceback
+import logging
 import requests
 import json
 from utils import get_dateselector_ui, create_xml_form_v3
@@ -16,19 +20,19 @@ from qwc_services_core.api import CaseInsensitiveArgument
 from qwc_services_core.tenant_handler import TenantHandler
 from qwc_services_core.runtime_config import RuntimeConfig
 from qwc_services_core.database import DatabaseEngine
-from sqlalchemy import text
+from sqlalchemy import text, exc
 
 # from PyQt5.QtDesigner import QFormBuilder
 
 # Flask application
 app = Flask(__name__)
 app_nocache(app)
-api = Api(app, version='1.0', title='MapInfo service API',
-          description="""API for QWC MapInfo service.
+api = Api(app, version='1.0', title='Date Selector service API',
+          description="""API for QWC GwDateSelector service.
 
-Additional information at a geographic position displayed with right mouse click on map.
+Functions to work with visit dates.
           """,
-          default_label='MapInfo operations', doc='/api/')
+          default_label='GwDateSelector operations', doc='/api/')
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
 # disable verbose 404 error message
@@ -38,19 +42,10 @@ auth = auth_manager(app, api)
 
 tenant_handler = TenantHandler(app.logger)
 
-getdates_parser = reqparse.RequestParser(argument_class=CaseInsensitiveArgument)
-getdates_parser.add_argument('theme', required=False)
-# getdates_parser.add_argument('layers', required=True)
-
-setdates_parser = reqparse.RequestParser(argument_class=CaseInsensitiveArgument)
-setdates_parser.add_argument('theme', required=True)
-setdates_parser.add_argument('dateFrom', required=True)
-setdates_parser.add_argument('dateTo', required=True)
-# setdates_parser.add_argument('layers', required=True)
 
 def get_config() -> RuntimeConfig:
     tenant = tenant_handler.tenant()
-    config_handler = RuntimeConfig("gwSelector", app.logger)
+    config_handler = RuntimeConfig("gwDateselector", app.logger)
     return config_handler.tenant_config(tenant)
 
 def get_db() -> DatabaseEngine:
@@ -105,6 +100,70 @@ def get_db_layers(theme: str) -> List[str]:
         db_layers = theme.get("layers")
     return db_layers
 
+# Create log pointer
+def create_log(self):
+    print(f"Tenant_name -> {tenant_handler.tenant()}")
+    today = date.today()
+    today = today.strftime("%Y%m%d")
+
+    # Directory where log file is saved, changes location depending on what tenant is used
+    tenant_directory = f"/var/log/qwc2-giswater-services/{tenant_handler.tenant()}"
+    if not os.path.exists(tenant_directory):
+        os.makedirs(tenant_directory)
+    
+    # Check if today's direcotry is created
+    today_directory = f"{tenant_directory}/{today}"
+    if not os.path.exists(today_directory):
+        os.makedirs(today_directory)
+    
+    service_name = os.getcwd().split(os.sep)[-1]
+    # Select file name for the log
+    log_file = f"{service_name}_{today}.log"
+
+    fileh = logging.FileHandler(f"{today_directory}/{log_file}", "a", encoding="utf-8")
+    # Declares how log info is added to the file
+    formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s", datefmt = "%d/%m/%y %H:%M:%S")
+    fileh.setFormatter(formatter)
+
+    # Removes previous handlers on root Logger
+    remove_handlers()
+    # Gets root Logger and add handdler
+    log = logging.getLogger()
+    log.addHandler(fileh)
+    log.setLevel(logging.DEBUG)
+    log.info(f" Executing class {self.__class__.__name__}")
+    return log
+
+# Removes previous handlers on root Logger
+def remove_handlers():
+    log = logging.getLogger()
+    for hdlr in log.handlers[:]:
+        log.removeHandler(hdlr)
+
+
+@api.route('/getdialog')
+class GwGetDialog(Resource):
+    @optional_auth
+    def get(self):
+        """Submit query
+
+        Returns dateselector dialog.
+        """
+        # Get dialog
+        form_xml = get_dateselector_ui()
+
+        # Response
+        response = {
+            "feature": {},
+            "data": {},
+            "form_xml": form_xml
+        }
+        remove_handlers()
+        return jsonify(response)
+
+
+getdates_parser = reqparse.RequestParser(argument_class=CaseInsensitiveArgument)
+getdates_parser.add_argument('theme', required=False)
 @api.route('/getdates')
 class GwGetDates(Resource):
     @api.expect(getdates_parser)
@@ -112,58 +171,54 @@ class GwGetDates(Resource):
     def get(self):
         """Submit query
 
-        Returns additional information at clicked map position.
+        Get current user's dates.
         """
-        """ not used atm
 
-        args = getdates_parser.parse_args()
-
-        identity = get_identity()
-        print(f"IDENTITY = {identity}")
+        # Get current dates
         config = get_config()
-        db = get_db()
+        log = create_log(self)
+        args = getdates_parser.parse_args()
+        identity = get_identity()
+        try:
+            db = get_db()
+        except:
+            remove_handlers() 
+
         schema = get_schema_from_theme(args["theme"], config)
         if schema is None:
+            log.warning(" Schema is None")
             return jsonify({"schema": schema})
 
-        """
+        log.info(f" Selected schema -> {str(schema)}")
 
-        form_xml = get_dateselector_ui()
+        dates = [None, None]
+        with db.begin() as conn:
+            sql = (f"SELECT from_date, to_date FROM {schema}.selector_date"
+                f" WHERE cur_user = '{identity}'")
+            log.info(f" Server execution -> {sql}")
+            print(f"SERVER EXECUTION: {sql}\n")
+            row = conn.execute(text(sql)).fetchone()
+            if row:
+                dates = [row[0].strftime("%d/%m/%Y"), row[1].strftime("%d/%m/%Y")]
+
+        # Response
         response = {
             "feature": {},
-            "data": {},
-            "form_xml": form_xml
-        }
-        return jsonify(response)
-        # layers = args['layers'].split(',')
-        layers = parse_layers(args["layers"], config, args["theme"])
-
-        request =  {
-            "client": {
-                "device": 5, "infoType": 1, "lang": "en_US", "epsg": int(args['epsg']), "cur_user": str(identity)
-            }, 
-            "form": {
-                "currentTab": str(args["currentTab"])
-            }, 
-            "feature": {},
             "data": {
-                "layers": layers,
-                "filterFields": {},
-                "pageInfo": {},
-                "selectorType": str(args["selectorType"]),
-                "filterText": "",
-                "loadProject": args["loadProject"]
-            }
+                "date_from": dates[0],
+                "date_to": dates[1]
+            },
+            "form_xml": None
         }
-        request_json = json.dumps(request)
-        sql = f"SELECT {schema}.gw_fct_getselectors($${request_json}$$);"
-        print(f"SERVER EXECUTION: {sql}\n")
-        with db.begin() as conn:
-            result: dict = conn.execute(text(sql)).fetchone()[0]
-            print(f"SERVER RESPONSE: {result}\n\n")
-            return handle_db_result(result, args["theme"])
+        remove_handlers()
+        return jsonify(response)
 
 
+setdates_parser = reqparse.RequestParser(argument_class=CaseInsensitiveArgument)
+setdates_parser.add_argument('theme', required=True)
+setdates_parser.add_argument('dateFrom', required=True)
+setdates_parser.add_argument('dateTo', required=True)
+# setdates_parser.add_argument('layers', required=True)
 @api.route('/setdates')
 class GwSetDates(Resource):
     @api.expect(setdates_parser)
@@ -171,39 +226,61 @@ class GwSetDates(Resource):
     def get(self):
         """Submit query
 
-        Returns additional information at clicked map position.
+        Updates current user's dates and returns them.
         """
-        args = setdates_parser.parse_args()
-
-        identity = get_identity()
-        print(f"IDENTITY = {identity}")
         config = get_config()
-        db = get_db()
+        log = create_log(self)
+        args = setdates_parser.parse_args()
+        identity = get_identity()
+        try:
+            db = get_db()
+        except:
+            remove_handlers() 
+
         schema = get_schema_from_theme(args["theme"], config)
         if schema is None:
+            log.warning(" Schema is None")
             return jsonify({"schema": schema})
+
+        log.info(f" Selected schema -> {str(schema)}")
 
         from_date = args["dateFrom"]
         to_date = args["dateTo"]
         with db.begin() as conn:
             sql = (f"SELECT * FROM {schema}.selector_date"
                 f" WHERE cur_user = '{identity}'")
+            log.info(f" Server execution -> {sql}")
             print(f"SERVER EXECUTION: {sql}\n")
             row = conn.execute(text(sql)).fetchone()
             if not row:
+                log.info(" Not row")
                 sql = (f"INSERT INTO {schema}.selector_date"
                     f" (from_date, to_date, context, cur_user)"
                     f" VALUES('{from_date}', '{to_date}', 'om_visit', '{identity}')")
             else:
+                log.info(f" Contains a row")
                 sql = (f"UPDATE {schema}.selector_date"
                     f" SET (from_date, to_date) = ('{from_date}', '{to_date}')"
                     f" WHERE cur_user = '{identity}'")
+            log.info(f" Server execution -> {sql}")
             print(f"SERVER EXECUTION: {sql}\n")
             conn.execute(text(sql))
 
-            # result: dict = conn.execute(text(sql)).fetchone()[0]
-            # print(f"SERVER RESPONSE: {result}\n\n")
-            # return handle_db_result(result, args["theme"])
+        # Response
+        from_split = from_date.split("-")
+        from_date = f"{from_split[2]}/{from_split[1]}/{from_split[0]}"
+        to_split = to_date.split("-")
+        to_date = f"{to_split[2]}/{to_split[1]}/{to_split[0]}"
+        response = {
+            "feature": {},
+            "data": {
+                "date_from": from_date,
+                "date_to": to_date
+            },
+            "form_xml": None
+        }
+        remove_handlers()
+        return jsonify(response)
 
 
 """ readyness probe endpoint """
@@ -220,6 +297,6 @@ def healthz():
 
 # local webserver
 if __name__ == '__main__':
-    print("Starting Giswater Selector service...")
+    print("Starting Giswater Date Selector service...")
     app.run(host='localhost', port=5017, debug=True)
 
