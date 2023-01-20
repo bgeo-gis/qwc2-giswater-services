4f2aaf107fc0e2c6513eb95d94f271b2c389a889
smaspons <smaspons@bgeo.es>
2023-01-20 14:28:01 +0000

    Selector: Add logging messages

--- gw-selector-service/server.py
+++ gw-selector-service/server.py
@@ -1,9 +1,13 @@
+from datetime import date
 from typing import List, Optional
 from xml.dom.minidom import parseString, Document, Element
 
 from flask import Flask, request, jsonify, Response
 from flask_restx import Resource, reqparse
+import os
+import traceback
 import requests
+import logging
 import json
 from utils import create_xml_form_v3
 
@@ -16,7 +20,7 @@ from qwc_services_core.api import CaseInsensitiveArgument
 from qwc_services_core.tenant_handler import TenantHandler
 from qwc_services_core.runtime_config import RuntimeConfig
 from qwc_services_core.database import DatabaseEngine
-from sqlalchemy import text
+from sqlalchemy import text, exc
 
 # from PyQt5.QtDesigner import QFormBuilder
 
@@ -35,7 +39,6 @@ app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 app.config['ERROR_404_HELP'] = False
 
 auth = auth_manager(app, api)
-
 tenant_handler = TenantHandler(app.logger)
 
 getselector_parser = reqparse.RequestParser(argument_class=CaseInsensitiveArgument)
@@ -65,6 +68,8 @@ def get_config() -> RuntimeConfig:
 
 def get_db() -> DatabaseEngine:
     print(f"DB URL: {get_config().get('db_url')}")
+    logging.basicConfig
+    logging.info(f"DB URL: {get_config().get('db_url')}")
     return DatabaseEngine().db_engine(get_config().get("db_url"))
 
 def get_schema_from_theme(theme: str, config: RuntimeConfig) -> Optional[str]:
@@ -77,6 +82,7 @@ def get_schema_from_theme(theme: str, config: RuntimeConfig) -> Optional[str]:
 def handle_db_result(result: dict, theme: str) -> Response:
     response = {}
     if not result:
+        logging.warning(" DB returned null")
         return jsonify({"message": "DB returned null"})
     if 'results' not in result or result['results'] > 0:
         form_xml = create_xml_form_v3(result)
@@ -115,6 +121,46 @@ def get_db_layers(theme: str) -> List[str]:
         db_layers = theme.get("layers")
     return db_layers
 
+# Create log pointer
+def create_log(self):
+    print(f"Tenant_name -> {tenant_handler.tenant()}")
+    today = date.today()
+    today = today.strftime("%Y%m%d")
+
+    # Directory where log file is saved, changes location depending on what tenant is used
+    tenant_directory = f"/var/log/qwc2-giswater-services/{tenant_handler.tenant()}"
+    if not os.path.exists(tenant_directory):
+        os.makedirs(tenant_directory)
+    
+    # Check if today's direcotry is created
+    today_directory = f"{tenant_directory}/{today}"
+    if not os.path.exists(today_directory):
+        os.makedirs(today_directory)
+    
+    service_name = os.getcwd().split(os.sep)[-1]
+    # Select file name for the log
+    log_file = f"{service_name}_{today}.log"
+
+    fileh = logging.FileHandler(f"{today_directory}/{log_file}", "a", encoding="utf-8")
+    # Declares how log info is added to the file
+    formatter = logging.Formatter("%(asctime)s %(levelname)s:%(message)s", datefmt = "%d/%m/%y %H:%M:%S")
+    fileh.setFormatter(formatter)
+
+    # Removes previous handlers on root Logger
+    remove_handlers()
+    # Gets root Logger and add handdler
+    log = logging.getLogger()
+    log.addHandler(fileh)
+    log.setLevel(logging.DEBUG)
+    log.info(f" Executing class {self.__class__.__name__}")
+    return log
+
+# Removes previous handlers on root Logger
+def remove_handlers():
+    log = logging.getLogger()
+    for hdlr in log.handlers[:]:
+        log.removeHandler(hdlr)
+
 @api.route('/getselector')
 class GwGetSelector(Resource):
     @api.expect(getselector_parser)
@@ -124,15 +170,23 @@ class GwGetSelector(Resource):
 
         Returns additional information at clicked map position.
         """
-        args = getselector_parser.parse_args()
-
-        identity = get_identity()
-        print(f"IDENTITY = {identity}")
         config = get_config()
-        db = get_db()
+        log = create_log(self)
+        args = getselector_parser.parse_args()
+        identity = get_identity()
+        try:
+            db = get_db()
+        except:
+            remove_handlers() 
+
         schema = get_schema_from_theme(args["theme"], config)
+
         if schema is None:
+            log.warning(" Schema is None")
+            remove_handlers()
             return jsonify({"schema": schema})
+
+        log.info(f" Selected schema {str(schema)}")
 
         # layers = args['layers'].split(',')
         layers = parse_layers(args["layers"], config, args["theme"])
@@ -156,10 +210,19 @@ class GwGetSelector(Resource):
         }
         request_json = json.dumps(request)
         sql = f"SELECT {schema}.gw_fct_getselectors($${request_json}$$);"
+        log.info(f" Server execution -> {sql}")
         print(f"SERVER EXECUTION: {sql}\n")
         with db.begin() as conn:
-            result: dict = conn.execute(text(sql)).fetchone()[0]
-            print(f"SERVER RESPONSE: {result}\n\n")
+            result = dict()
+            try:
+                result = conn.execute(text(sql)).fetchone()[0]
+            except exc.ProgrammingError:
+                log.warning(" Server execution failed")
+                print(f"Server execution failed\n{traceback.format_exc()}")
+                remove_handlers()
+            log.info(f" Server response -> {json.dumps(result)}")
+            print(f"SERVER RESPONSE: {json.dumps(result)}\n")
+            remove_handlers()
             return handle_db_result(result, args["theme"])
 
 
@@ -172,15 +235,22 @@ class GwSetSelector(Resource):
 
         Returns additional information at clicked map position.
         """
-        args = setselector_parser.parse_args()
-
-        identity = get_identity()
-        print(f"IDENTITY = {identity}")
         config = get_config()
-        db = get_db()
+        log = create_log(self)
+        args = setselector_parser.parse_args()
+        identity = get_identity()
+        try:
+            db = get_db()
+        except:
+            remove_handlers()
+        
         schema = get_schema_from_theme(args["theme"], config)
         if schema is None:
+            log.warning(f" Schema is None")
+            remove_handlers()
             return jsonify({"schema": schema})
+
+        log.info(f" Selected schema {str(schema)}")
 
         # layers = args['layers'].split(',')
         layers = parse_layers(args["layers"], config, args["theme"])
@@ -210,10 +280,19 @@ class GwSetSelector(Resource):
 
         request_json = json.dumps(request)
         sql = f"SELECT {schema}.gw_fct_setselectors($${request_json}$$);"
+        log.info(f" Server execution -> {sql}")
         print(f"SERVER EXECUTION: {sql}\n")
         with db.begin() as conn:
-            result: dict = conn.execute(text(sql)).fetchone()[0]
+            result = dict()
+            try:
+                result = conn.execute(text(sql)).fetchone()[0]
+            except exc.ProgrammingError:
+                log.warning(" Server execution failed")
+                print(f"Server execution failed\n{traceback.format_exc()}")
+                remove_handlers()
+            log.info(f" Server response: {str(result)[0:100]}")
             print(f"SERVER RESPONSE: {result}\n\n")
+            remove_handlers()
             return handle_db_result(result, args["theme"])
 
 
