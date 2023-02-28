813146476ebd4799cb5440e319eec7025129e3da
smaspons <smaspons@bgeo.es>
2023-02-28 10:24:38 +0000

    Dateselector: harmonize return keys

--- dateselector/dateselector_utils.py
+++ dateselector/dateselector_utils.py
@@ -5,7 +5,8 @@ from flask import jsonify, Response
 def handle_db_result(result: dict, theme: str) -> Response:
     response = {}
     if not result:
-        return jsonify({"message": "DB returned null"})
+        response = utils.create_response(result)
+        return jsonify(response)
     if 'results' not in result or result['results'] > 0:
         layer_columns = {}
         db_layers = utils.get_db_layers(theme)
@@ -13,19 +14,10 @@ def handle_db_result(result: dict, theme: str) -> Response:
         for k, v in db_layers.items():
             if v in result['body']['data']['layerColumns']:
                 layer_columns[k] = result['body']['data']['layerColumns'][v]
+        
+        response = utils.create_response(result)
+        response["body"]["data"]["layerColumns"] = layer_columns
 
-        response = {
-            "feature": {},
-            "data": {
-                #"userValues": result['body']['data']['userValues'],
-                #"geometry": result['body']['data']['geometry'],
-                "date_from": result['body']['data']['date_from'],
-                "date_to": result['body']['data']['date_to'],
-                "layerColumns": layer_columns
-            },
-            "form": {},
-            "form_xml": None
-        }
     return jsonify(response)
 
 
