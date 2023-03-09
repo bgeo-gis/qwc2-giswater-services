e4d3f9ed6992c463d561984963eb4aa269791eec
smaspons <smaspons@bgeo.es>
2023-03-09 12:00:35 +0000

    Flowtrace: use execute_procedure & clean code

--- flowtrace/flowtrace.py
+++ flowtrace/flowtrace.py
@@ -1,12 +1,7 @@
 import utils
-from .flowtrace_utils import handle_db_result
 
-import json
-import traceback
-
-from flask import Blueprint, request, jsonify
-from qwc_services_core.auth import optional_auth, get_identity
-from sqlalchemy import text, exc
+from flask import Blueprint, request
+from qwc_services_core.auth import optional_auth
 
 flowtrace_bp = Blueprint('flowtrace', __name__)
 
@@ -14,16 +9,11 @@ flowtrace_bp = Blueprint('flowtrace', __name__)
 @flowtrace_bp.route('/upstream', methods=['GET'])
 @optional_auth
 def upstream():
-    """Submit query
+    """Upstream
 
-    Returns additional information at clicked map position.
+    Runs flowtrace upstream at clicked map position.
     """
-    config = utils.get_config()
     log = utils.create_log(__name__)
-    try:
-        db = utils.get_db()
-    except:
-        utils.remove_handlers()
 
     # args
     args = request.get_json(force=True) if request.is_json else request.args
@@ -32,65 +22,23 @@ def upstream():
     coords = args.get("coords").split(',')
     zoom = args.get("zoom")
 
-    schema = utils.get_schema_from_theme(theme, config)
+    # db fct
+    coordinates = f'"epsg": {int(epsg)}, "xcoord": {coords[0]}, "ycoord": {coords[1]}, "zoomRatio": {float(zoom)}'
+    extras = f'"coordinates": {{{coordinates}}}'
+    body = utils.create_body(extras=extras)
+    result = utils.execute_procedure(log, theme, 'gw_fct_graphanalytics_upstream', body)
 
-    if schema is None:
-        log.warning("Schema is None")
-        utils.remove_handlers()
-        return jsonify({"schema": schema})
-
-    log.info(f"Selected schema -> {str(schema)}")
-
-    request_json =  {
-        "client": {
-            "device": 5, "infoType": 1, "lang": "en_US", "epsg": int(epsg)
-        }, 
-        "form": {}, 
-        "feature": {
-            
-        },
-        "data": {
-            "filterFields": {},
-            "pageInfo": {},
-            "coordinates": {
-                "epsg": int(epsg),
-                "xcoord": coords[0],
-                "ycoord": coords[1],
-                "zoomRatio": float(zoom)
-            }
-        }
-    }
-    request_json = json.dumps(request_json)
-    sql = f"SELECT {schema}.gw_fct_graphanalytics_upstream($${request_json}$$);"
-    log.info(f" Server execution -> {sql}")
-    print(f"SERVER EXECUTION: {sql}\n")
-    with db.begin() as conn:
-        result = dict()
-        try:
-            result = conn.execute(text(sql)).fetchone()[0]
-        except exc.ProgrammingError:
-            log.warning(" Server execution failed")
-            print(f"Server execution failed\n{traceback.format_exc()}")
-            utils.remove_handlers()
-        log.info(f" Server response: {str(result)[0:100]}")
-        print(f"SERVER RESPONSE: {result}\n\n")
-        utils.remove_handlers()
-        return handle_db_result(result)
+    return utils.create_response(result, do_jsonify=True)
 
 
 @flowtrace_bp.route('/downstream', methods=['GET'])
 @optional_auth
 def downstream():
-    """Submit query
+    """Downstream
 
-    Returns additional information at clicked map position.
+    Runs flowtrace downstream at clicked map position.
     """
-    config = utils.get_config()
     log = utils.create_log(__name__)
-    try:
-        db = utils.get_db()
-    except:
-        utils.remove_handlers()
 
     # args
     args = request.get_json(force=True) if request.is_json else request.args
@@ -99,47 +47,10 @@ def downstream():
     coords = args.get("coords").split(',')
     zoom = args.get("zoom")
 
-    schema = utils.get_schema_from_theme(theme, config)
+    # db fct
+    coordinates = f'"epsg": {int(epsg)}, "xcoord": {coords[0]}, "ycoord": {coords[1]}, "zoomRatio": {float(zoom)}'
+    extras = f'"coordinates": {{{coordinates}}}'
+    body = utils.create_body(extras=extras)
+    result = utils.execute_procedure(log, theme, 'gw_fct_graphanalytics_downstream', body)
 
-    if schema is None:
-        log.warning(" Schema is None")
-        utils.remove_handlers()
-        return jsonify({"schema": schema})
-
-    log.info(f" Selected schema -> {str(schema)}")
-
-    request_json =  {
-        "client": {
-            "device": 5, "infoType": 1, "lang": "en_US", "epsg": int(epsg)
-        }, 
-        "form": {}, 
-        "feature": {
-            
-        },
-        "data": {
-            "filterFields": {},
-            "pageInfo": {},
-            "coordinates": {
-                "epsg": int(epsg),
-                "xcoord": coords[0],
-                "ycoord": coords[1],
-                "zoomRatio": float(zoom)
-            }
-        }
-    }
-    request_json = json.dumps(request_json)
-    sql = f"SELECT {schema}.gw_fct_graphanalytics_downstream($${request_json}$$);"
-    log.info(f" Server execution -> {sql}")
-    print(f"SERVER EXECUTION: {sql}\n")
-    with db.begin() as conn:
-        result = dict()
-        try:
-            result = conn.execute(text(sql)).fetchone()[0]
-        except exc.ProgrammingError:
-            log.warning(" Server execution failed")
-            print(f"Server execution failed\n{traceback.format_exc()}")
-            utils.remove_handlers()
-        log.info(f" Server response: {str(result)[0:100]}")
-        print(f"SERVER RESPONSE: {result}\n\n")
-        utils.remove_handlers()
-        return handle_db_result(result)
+    return utils.create_response(result, do_jsonify=True)
--- flowtrace/flowtrace_utils.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify, Response
-
-def handle_db_result(result: dict) -> Response:
-    response = {}
-    if 'results' not in result or result['results'] > 0:
-        response = result
-    return jsonify(response)
