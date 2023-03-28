d0ccb0cf1793b3eccd85ce555262d9927159b02d
Edgar Fuste <efuste@bgeo.es>
2023-03-28 09:16:10 +0200

    Enhancement: add GNU headers

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+"""
+Copyright BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+# -*- coding: utf-8 -*-
+
 from typing import Optional, List
 from datetime import date
 from flask import jsonify
