8fd778f479df1efcf99942e2a2066ea6bf3da94d
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-08-30 10:35:19 +0200

    Update headers files

--- tiling/tiling.py
+++ tiling/tiling.py
@@ -1,9 +1,9 @@
 """
- Copyright BGEO. All rights reserved.
- The program is free software: you can redistribute it and/or modify it under the terms of the GNU
- General Public License as published by the Free Software Foundation, either version 3 of the License,
- or (at your option) any later version
-""" 
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
 
 # DEPRECATED: Now inside mapproxy-service
 
--- toolbox/toolbox_process.py
+++ toolbox/toolbox_process.py
@@ -1,10 +1,11 @@
-from flask import jsonify, Response
 """
- Copyright BGEO. All rights reserved.
- The program is free software: you can redistribute it and/or modify it under the terms of the GNU
- General Public License as published by the Free Software Foundation, either version 3 of the License,
- or (at your option) any later version
-""" 
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+
+from flask import jsonify, Response
 import logging
 import json
 
